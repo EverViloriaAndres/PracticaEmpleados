7a3daeca59be3bad157e809ac5b241044c4aa1d2
eViloria <evanvineevanvine@gmail.com>
2023-06-29 06:06:03 -0500

    Se agrega el uso de servicios

--- src/Modulo1/Modulo1.module.ts
+++ src/Modulo1/Modulo1.module.ts
@@ -5,9 +5,20 @@ import { ComponentePrincipal } from './Empleados/Empleados.component';
 import { ComponentHijo } from './ComponentHijo/ComponentHijo.componente';
 import { FormsModule } from '@angular/forms';
 import { PrimerServicioService } from './primer-servicio.service';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 
 @NgModule({
-  declarations: [ComponentePrincipal, ComponentHijo],
+  declarations: [
+    ComponentePrincipal,
+    ComponentHijo,
+    ContactoComponent,
+    HomeComponent,
+    ProyectosComponent,
+    QuienesSomosComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [PrimerServicioService],
   bootstrap: [ComponentePrincipal, ComponentHijo],
